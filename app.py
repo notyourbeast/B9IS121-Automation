dc5b50e60263c518989a6fa0d13cd293d9fc7adc
Saiprasad Shiragave <saishiragave@gmail.com>
2025-11-08 22:02:08 +0000

    demovid

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Docker container Demo2 !"
+    return "Docker container Network Systems assignment !"
 
 
 if __name__ == "__main__":
